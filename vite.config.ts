662e3bac955ca038ae91ed54c631f13f10e41020
alexouuuuuuuuuuu <alexis.maugain@etu.unilim.fr>
2025-10-23 11:08:32 +0200

    add

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Remplacez par le nom de votre repository
+  base: '/portfolio/', 
   resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
