31c0c4baa8f1f94d1d5cd6239b344d4662d99bd5
alexouuuuuuuuuuu <alexis.maugain@etu.unilim.fr>
2025-12-10 14:03:17 +0100

    add

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -15,7 +15,7 @@ const projects = [
     technologies: ["Figma", "Prototyping", "User Research"],
     categories: ["UI/UX"], 
     image: "./assets/projets/olive-oil.png",
-    link: "Oliveoil.html",
+    link: "./Oliveoil.html",
     size: "large",
   },
   {
